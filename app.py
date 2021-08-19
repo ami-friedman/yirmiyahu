ba47d94cf098ec9cf0ab4370831062fe419599d2
Ami Friedman <ami-friedman@gmail.com>
2021-08-19 15:58:12 +0300

    Revert "Revert "added login""
    This reverts commit 74d10f8be117041a2c81c3311bc4413391e5a85f.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import json
 from flask import request, jsonify, make_response
 from flask_cors import CORS
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
 
+from canon import Auth
 from model_wrappers import books_wrapper, authors_wrapper, subs_wrapper, loans_wrapper, \
     subscriptions_wrapper, categories_wrapper, book_types_wrapper
 
@@ -8,7 +12,6 @@ from model_wrappers import books_wrapper, authors_wrapper, subs_wrapper, loans_w
 from models import app
 
 CORS(app, resources={r'/*': {'origins': '*'}})
-
 
 @app.route('/books', methods=['GET', 'POST'])
 def books():
